f064ee02bc1b164dac6bd3362695383d499d71d7
이원찬 <twoone14@tukorea.ac.kr>
2024-09-15 22:46:59 +0900

    feat: 에러 발생시 에러 발생 채널에 알림하는 ErrorManager Class 구현

---
+++ src/error.py
@@ -0,0 +1,18 @@
+from src.notification import Notification
+
+
+class ErrorManager:
+    error_count = 0
+
+    def __init__(self, notification: Notification):
+        self.notification = notification
+
+    def increase_error_count(self, error: Exception):
+        print(error)
+        self.error_count += 1
+        self.notification.send_error_message(f"에러가 발생했습니다. {error}")
+
+        if self.error_count >= 3:
+            self.notification.send_error_message(f"세번이상 발생한 오류입니다. {error}")
+
+        return self.error_count
