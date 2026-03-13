faa3780fc97d35cf67875c738d1f9eb93de7f636
Leewonchan <twoone14@tukorea.ac.kr>
2026-03-13 14:00:57 +0900

    fix: crawling cron optimize

--- src/schedule/index.ts
+++ src/schedule/index.ts
@@ -46,28 +46,28 @@ export const scheduleTasks = async () => {
   // Notice
   withErorrWebHook(() => noticeCrawler(getPage))();
   cron.schedule(
-    "*/5 * * * *",
+    "0-59/10 * * * *", // 10분 마다 
     withErorrWebHook(() => noticeCrawler(getPage))
   );
 
   // Dormitory Notice
   withErorrWebHook(() => dormitoryNoticeCrawler(getPage))();
   cron.schedule(
-    "*/5 * * * *",
+    "3-59/15 * * * *", // 15분 마다 
     withErorrWebHook(() => dormitoryNoticeCrawler(getPage))
   );
 
   // School Meal
   withErorrWebHook(() => schoolMealCrawler(getPage))();
   cron.schedule(
-    "*/5 * * * *",
+    "6-59/30 * * * *", // 30분 마다
     withErorrWebHook(() => schoolMealCrawler(getPage))
   );
 
   // Shuttle
   withErorrWebHook(() => shuttleCrawler(getPage))();
   cron.schedule(
-    "*/5 * * * *",
+    "9-59/60 * * * *", // 1시간 마다
     withErorrWebHook(() => shuttleCrawler(getPage))
   );
 };
